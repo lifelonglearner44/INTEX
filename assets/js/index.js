a6b020d59ba9d0b6819bddc1465ce27bd8b41677
McKay Boody <mgboody@gmail.com>
2023-12-04 16:21:24 -0700

    update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,11 +8,12 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     // If we use AWS, the host will be different
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'bucket_list',
-    port: 5432,
+    host: process.env.RDS_HOSTNAME || 'localhost',
+    user: process.env.RDS_USERNAME || 'postgres',
+    password: process.env.RDS_PASSWORD || 'admin',
+    database: process.env.RDS_PORT || 'bucket_list',
+    port: process.env.RDS_PORT || 5432,
+    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   },
 });
 app.get('/', (req, res) => {

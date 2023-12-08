f34b5e5011a42d70f836db7ebf124776f5083509
Your Name <you@example.com>
2023-12-08 02:15:17 -0700

    cert

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d provomentalhealth11.is404.net --nginx --agree-tos --email nrd26@byu.edu
+sudo certbot -n -d socialmedia11.is404.net --nginx --agree-tos --email nrd26@byu.edu

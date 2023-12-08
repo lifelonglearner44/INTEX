bef5ba5201e3ccb53f296c8216e718ea41636952
McKay Boody <mgboody@gmail.com>
2023-12-07 20:29:20 -0700

    updates

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const session = require('express-session');
+
 let app = express();
 var path = require('path');
+const bodyParser = require('body-parser');
 
 const port = 3000;
+
+// Set up express-session middleware
+app.use(
+  session({
+    secret: 'your-secret-key', // Replace with a strong secret key
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Your authentication middleware
+const authenticate = (req, res, next) => {
+  if (req.session && req.session.loggedIn) {
+    // User is logged in, continue to the next middleware
+    next();
+  } else {
+    // User is not logged in, redirect to the login page
+    res.redirect('/index.html'); // Replace '/login' with your login page route
+  }
+};
 
 app.set('view engine', 'ejs');
 
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,14 +52,36 @@ const knex = require('knex')({
   },
 });
 
+// landing page views with ejs and authentication
+
+// app.get('/adminIndex.ejs', authenticate, async (req, res) => {
+//   // const isAuthenticated = req.isAuthenticated();
+//   const isAuthenticated = req.session.loggedIn;
+//   console.log('isAuthenticated', isAuthenticated);
+//   // Render the landing page with authentication status
+//   res.render('adminIndex', { isAuthenticated });
+// });
+
+// // landing page view
+// app.get('/index.html', authenticate, async (req, res) => {
+//   const isAuthenticated = req.authenticate();
+
+//   // Render the landing page with authentication status
+//   res.render('index', { isAuthenticated });
+// });
+
 // landing page view
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
-
 // landing page view
 app.get('/index.html', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// dashboard page view
+app.get('/dashboard.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dashboard.html'));
 });
 
 // FAQ page view
@@ -52,16 +99,111 @@ app.get('/survey.html', (req, res) => {
   res.sendFile(path.join(__dirname, '/survey.html'));
 });
 
-// new admin report page view with ejs
-app.get('/adminReports', async (req, res) => {
-  try {
-    // Execute the query and wait for the results
-    const data = await knex.select().from('Test');
+// login page view
+app.get('/pages-login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages-login.html'));
+});
+// admin register page view
+app.get('/adminRegister.ejs', authenticate, (req, res) => {
+  const isAuthenticated = req.session.loggedIn;
+  if (isAuthenticated) {
+    res.render('adminRegister', { isAuthenticated });
+  } else if (!isAuthenticated) {
+    res.redirect('/pages-login.html');
+  }
+});
+// admin profile page view
+app.get('/users-profile.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/users-profile.html'));
+});
 
-    // Render the EJS template and pass the data to it
-    res.render('adminReports', { data });
+// ,
+
+// new admin report page view with ejs
+app.get('/adminReport.ejs', authenticate, async (req, res) => {
+  try {
+    const isAuthenticated = req.session.loggedIn;
+
+    // Fetch data from the database using knex if needed
+    const data = await knex.select().from('Survey_Responses');
+
+    // Render the EJS template
+    res.render('adminReport', { data, isAuthenticated });
+    /* pass data to the template if needed */
   } catch (error) {
     console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// login
+app.post('/loginAttempt', async (req, res) => {
+  try {
+    console.log('req.body:', req.body);
+    const { Username, Password } = req.body;
+
+    // Query the database to check if the provided credentials match a user
+
+    // *** remove the .first()
+    const user = await knex('User').where({ Username, Password }).first();
+    console.log('user', user);
+
+    if (user) {
+      // Set the session variable on successful login
+      req.session.loggedIn = true;
+      // res.send('Logged in successfully!');
+      res.redirect('/adminReport.ejs');
+      // if (user.Is_Admin === true) {
+      //   res.redirect('/pages-register.html');
+      // } else {
+      //   // If a user is found, redirect to the user profile page
+      //   res.redirect('/users-profile.html');
+      // }
+    } else {
+      // If no user is found, handle the authentication failure
+      res.status(401).send('Invalid username or password');
+      // res.redirect('/pages-login.html');
+    }
+  } catch (error) {
+    console.error('Database Query Error:', error);
+    // res.status(500).send('Internal Server Error');
+  }
+});
+
+// Example logout route
+app.get('/logout', (req, res) => {
+  // Clear the session variable on logout
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+    }
+    // Redirect to the login page after logout
+    res.redirect('/index.html'); // Replace '/login' with your login page route
+  });
+});
+// posting to the database
+app.post('/registerNewAccount', async (req, res) => {
+  try {
+    console.log('req.body:', req.body);
+    let { Username } = req.body;
+    const usernameCheck = await knex('User').where({ Username }).first();
+    console.log('usernameCheck', usernameCheck);
+    if (usernameCheck) {
+      res.status(401).send('Username already exists');
+    } else if (!usernameCheck) {
+      const User = {
+        First_Name: req.body.First_Name,
+        Last_Name: req.body.Last_Name,
+        Username: req.body.Username,
+        Email: req.body.Email,
+        Password: req.body.Password,
+      };
+      const surveyResult = await knex('User').insert(User);
+      console.log('Insert survey Result:', surveyResult);
+      res.send('User successfully created!');
+    }
+  } catch (error) {
+    console.error('Database Insert Error:', error);
     res.status(500).send('Internal Server Error');
   }
 });
@@ -70,6 +212,7 @@ app.get('/adminReports', async (req, res) => {
 app.post('/surveyPost', async (req, res) => {
   try {
     console.log('req.body:', req.body);
+    console.log('req', req);
     // console.log('req', req);
     const Survey_Responses = {
       Origin: 'Provo',
@@ -94,11 +237,23 @@ app.post('/surveyPost', async (req, res) => {
       Interest_In_Daily_Activities: req.body.Interest_In_Daily_Activities,
       Sleep_Issues: req.body.Sleep_Issues,
     };
+
+    const Main = {
+      Survey_Response_ID: 1,
+      Organization_ID: 1,
+      Social_Platform_ID: 1,
+    };
+
     console.log('Survey_Responses:', Survey_Responses);
 
-    const result = await knex('Survey_Responses').insert(Survey_Responses);
+    // const surveyResult = await knex('Survey_Responses').insert(
+    //   Survey_Responses
+    // );
+    // console.log('Insert survey Result:', surveyResult);
 
-    console.log('Insert Result:', result);
+    const mainResult = await knex('Main').insert(Main);
+
+    console.log('Main Result:', mainResult);
 
     // res.redirect('/index.html');
 
@@ -108,23 +263,6 @@ app.post('/surveyPost', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
-// with ejs
-
-// app.get('/', async (req, res) => {
-//   try {
-//     // Fetch data from the database using knex if needed
-//     // const data = await knex.select().from('Social_Platform_Information');
-
-//     // Render the EJS template
-//     res.render('index', {
-//       /* pass data to the template if needed */
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 
 // above with ejs
 app.listen(port, () =>
